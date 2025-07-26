36b26a3a0bfc7b4745554640cf937e57a5479fdb
김주현 <juhy0987@naver.com>
2025-07-27 04:24:39 +0900

    [FEAT]: 즐겨찾기 API 라우터 초기화 및 favorite_router 포함

--- locations/favorite/__init__.py
+++ locations/favorite/__init__.py
@@ -1,5 +1,15 @@
+from fastapi import APIRouter
+
+from .favorite_router import router as favorite_router
+
+router = APIRouter(prefix="/favorite")
+
+router.include_router(favorite_router)
+
+
 from .favorite import LocationFavorite
 
 __all__ = [
+    "router",
     "LocationFavorite",
 ]
