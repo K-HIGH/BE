4eafa85927197f30a81e6d894726219878b33ec7
김주현 <juhy0987@naver.com>
2025-07-27 02:56:38 +0900

    [FEAT]: 안전 구역 API 라우터 및 DTO 클래스 추가 - 안전 구역 목록 조회, 생성, 수정, 삭제 기능 구현

--- users/safety_areas/__init__.py
+++ users/safety_areas/__init__.py
@@ -1,7 +1,17 @@
+from fastapi import APIRouter
+
+from .safety_area_router import router as safety_area_router
+
+router = APIRouter(prefix="/safety-areas")
+
+router.include_router(safety_area_router)
+
+
 from .safety_area import SafetyArea
 from .safety_area_crud import safety_area_crud
 
 __all__ = [
+    "router",
     "SafetyArea",
     "safety_area_crud",
 ]
