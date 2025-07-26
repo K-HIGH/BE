a58cad7b10efe3a9f15ad143eafcc2b515b584d0
김주현 <juhy0987@naver.com>
2025-07-27 04:06:43 +0900

    [REFACTOR]: 불필요한 메시지 엔드포인트 및 관련 DTO 임포트 제거

--- root_router.py
+++ root_router.py
@@ -1,6 +1,4 @@
 from fastapi import APIRouter
-from common.basic_dtos.responses import MessageResponse
-from common.basic_dtos.requests import MessageRequest
 
 from users import router as users_router
 from auth import router as auth_router
@@ -13,13 +11,3 @@ root_router.include_router(users_router)
 root_router.include_router(auth_router)
 
 root_router.include_router(locations_router)
-
-
-# 예시 POST 엔드포인트
-@root_router.post("/message", response_model=MessageResponse, tags=["Root"])
-async def send_message(request: MessageRequest) -> MessageResponse:
-    """메시지를 받아서 응답을 반환하는 예시 엔드포인트"""
-    return MessageResponse(
-        received_message=request.message,
-        response=f"받은 메시지: '{request.message}'에 대한 응답입니다."
-    )
