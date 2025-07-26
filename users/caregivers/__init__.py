e4c91e6454990c780737c474faa4c0b5ecd5d386
김주현 <juhy0987@naver.com>
2025-07-27 03:59:12 +0900

    [FEAT]: 보호자 API 라우터 추가 및 caregiver_router 포함

--- users/caregivers/__init__.py
+++ users/caregivers/__init__.py
@@ -1,7 +1,16 @@
+from fastapi import APIRouter
+
+from .caregiver_router import router as caregiver_router
+
+router = APIRouter(prefix="/caregivers")
+
+router.include_router(caregiver_router)
+
 from .caregiver import Caregiver
 from .caregiver_crud import caregiver_crud
 
 __all__ = [
+    "router",
     "Caregiver",
     "caregiver_crud",
 ]
