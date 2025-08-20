0ec5bb1f1a0978ef01242b28def36ba4ed1640a8
juhy0987 <juhy0987@naver.com>
2025-08-21 01:07:54 +0900

    [REFACTOR]: memcache 클라이언트 초기화 시 pickle_serde를 사용하도록 변경

--- common/memcache/database.py
+++ common/memcache/database.py
@@ -1,3 +1,4 @@
 from pymemcache.client import Client
+from pymemcache.serde import pickle_serde
 
-memcache_client = Client(('localhost', 11211))
+memcache_client = Client(('localhost', 11211), serde=pickle_serde)
